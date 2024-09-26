c50e6b3fa6d46323632ca93c3cbd1678030605f5
mariamaaa12 <131659478+mariamaaa12@users.noreply.github.com>
2024-09-26 14:21:59 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,3 +10,7 @@ st.write("Ceci est un formulaire de contact")
 user_input =st.text_input("Tapez votre texte : ")
 
 st.write(user_input)
+
+# image
+
+st.image("https://www.google.com/imgres?q=eemi&imgurl=https%3A%2F%2Fwww.eemi.com%2Fuploads%2F2023%2F06%2Fsized%2FCampus-de-Paris-scaled-aspect-ratio-690-690-scaled-380x380.jpg&imgrefurl=https%3A%2F%2Fwww.eemi.com%2F&docid=dtSDTqB1srHmXM&tbnid=6hQXnPWtQ61KiM&vet=12ahUKEwjP1921zOCIAxXbTKQEHV0XLagQM3oECBgQAA..i&w=380&h=380&hcb=2&ved=2ahUKEwjP1921zOCIAxXbTKQEHV0XLagQM3oECBgQAA")
