e99119d3f971f33ee4639ad4fd68bda8a2005632
mariamaaa12 <131659478+mariamaaa12@users.noreply.github.com>
2024-09-26 15:39:17 +0200

    Update Dalle-e 3.py

--- pages/Dalle-e 3.py
+++ pages/Dalle-e 3.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from openai import OpenAI
 
 st.title("Dalle-e 3")
 st.write("Veillez entré une description de l'image que vous souhaiter générer ")
@@ -7,6 +8,28 @@ st.write(description )
 st.sidebar.title("Mariama Diallo")
 
 cle=st.sidebar.text_input("Veiller saisir votre clé")
+# OpenAI pour générer des images
+
+
+
+client = OpenAI(api_key=cle)
+
+
+# Testez ici plusieurs variation du prompte
+prompt = "A cute baby sea otter"
+
+
+
+image = client.images.generate(
+    model="dall-e-2",
+    prompt=prompt,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
+
+image_url = image.data[0].url
+print(image_url)
 
 
 
