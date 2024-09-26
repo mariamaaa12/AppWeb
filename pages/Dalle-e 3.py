ebf4e7d3d13e0a0ec5771ae377fe7ab61072881c
mariamaaa12 <131659478+mariamaaa12@users.noreply.github.com>
2024-09-26 15:27:36 +0200

    Update Dalle-e 3.py

--- pages/Dalle-e 3.py
+++ pages/Dalle-e 3.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.title("Dalle-e 3")
+st.write("Veillez entré une description de l'image que vous souhaiter générer ")
+description = st.text_input("veillez saisir votre déscription :")
+
+
