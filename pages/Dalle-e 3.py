3bf239c4dfa678e8a1306c0991071c0ea18897b9
mariamaaa12 <131659478+mariamaaa12@users.noreply.github.com>
2024-09-26 15:37:49 +0200

    Update Dalle-e 3.py

--- pages/Dalle-e 3.py
+++ pages/Dalle-e 3.py
@@ -4,6 +4,10 @@ st.title("Dalle-e 3")
 st.write("Veillez entré une description de l'image que vous souhaiter générer ")
 description = st.text_input("veillez saisir votre déscription :")
 st.write(description )
+st.sidebar.title("Mariama Diallo")
+
+cle=st.sidebar.text_input("Veiller saisir votre clé")
+
 
 
 
